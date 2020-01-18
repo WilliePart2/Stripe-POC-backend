a9c8690f679c29fb10f6ca346ab8504d5ee0b907
willie <williepart3@gmail.com>
2020-01-19 01:39:03 +0200

    add routes for transfer reversing

--- routes/transfers.js
+++ routes/transfers.js
@@ -54,8 +54,43 @@ router.get('/transfers/:id', commonWrapper(async (req, res) => {
     return await stripe.transfers.retrieve(transferId);
 }));
 
-router.get('/transfers', commonWrapper(async (req, res) => {
+router.get('/transfers', commonWrapper(({body}) => {
+    const startingAfter = body.startingAfter;
+    const finishedBefore = body.finishedBefore;
+    const limit = body.limit || 100;
+
+    return stripe.transfers.list({
+        limit,
+        starting_after: startingAfter,
+        ending_before: finishedBefore,
+    });
+}));
+
+router.get('/transfers/internal', commonWrapper(async (req, res) => {
     return transferContext.getAll();
+}));
+
+router.post('/transfers/:id/reversals', commonWrapper(({ params, body }) => {
+    const transferId = params.id;
+    const amount = body.amount;
+    const description = body.description;
+
+    return stripe.transfers.createReversal(transferId, {
+        amount,
+        description,
+        metadata: {
+            description,
+        },
+        refund_application_fee: false,
+    });
+}));
+
+router.get('/transfers/:id/reversals', commonWrapper((req) => {
+    const transferId = req.params.id;
+
+    return stripe.transfers.listReversals(transferId, {
+        limit: 100,
+    });
 }));
 
 module.exports = router;
